07d1c2edefc9f50c5f1de7d7e254a483e4504064
Larry Ruckman <ruckman@slac.stanford.edu>
2017-08-21 21:25:25 -0700

    added common_config.mk

---
+++ firmware/targets/common_config.mk
@@ -0,0 +1,10 @@
+export GIT_BYPASS = 0
+
+# Define Firmware Version Number
+export PRJ_VERSION = 0xCEC83100
+
+# Define target part
+export PRJ_PART = XC7A200TFFG1156-3
+
+# Define target output
+target: prom
